21935831d350d9db037b31c936c3547d8a5f5eb0
VICTOR-NB\Victor <victor.tin@gmail.com>
2020-11-12 17:09:14 +0800

    no comment

--- TestFun/src/J8DateTime.java
+++ TestFun/src/J8DateTime.java
@@ -5,12 +5,12 @@ import java.time.temporal.ChronoField;
 public class J8DateTime {
 
 	public static void main(String[] args) {
-		// 建立 LocalDate
-		// 獲取當前年月日
+		
+		
 		LocalDate localDate = LocalDate.now();
-		// 構造指定的年月日
+		
 		LocalDate localDate1 = LocalDate.of(2019, 9, 12);
-		// 獲取年、月、日、星期幾
+		
 		int year = localDate.getDayOfYear();//.getYear();
 		int year1 = localDate.get(ChronoField.YEAR);
 		System.out.println("year "+year);
